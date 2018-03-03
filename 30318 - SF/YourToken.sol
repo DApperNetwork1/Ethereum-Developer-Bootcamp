19b52f40e370d58fbb85ee32abc5f5f6f17f4a62
DApperNetwork <ericchung@thedappernetwork.com>
2018-03-03 01:58:33 -0800

    Update and rename StudentToken.sol to YourToken.sol

--- 30318 - SF/YourToken.sol
+++ 30318 - SF/YourToken.sol
@@ -6,7 +6,7 @@ import "./MintableToken.sol";
 /** Create contract that generates your very own custom ERC20 token with minting capabilities.
  * You'll be "selling" the tokens this contract generates to your classmates using your crowdsale contract.
  * Make sure to inherit functionality from the MintableToken contract.
- * Rename the contract file to the name of your token (i.e. EricToken.sol or BuidlDApps.sol or RocketCoin.sol)
+ * Rename the contract file to the name of your custom token (i.e. EricToken.sol or BuidlDApps.sol or RocketCoin.sol)
  */ 
 
 // Set up your contract. 

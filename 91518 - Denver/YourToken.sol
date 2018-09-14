d3d7c2ba59be6fa446379d2d090832d5fb8cc387
DApperNetwork <ericchung@thedappernetwork.com>
2018-09-14 12:13:39 -0600

    Create YourToken.sol

---
+++ 91518 - Denver/YourToken.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.24;
+
+// Import the MintableToken contract you just wrote.
+import "./MintableToken.sol";
+
+/** Create contract that generates your very own custom ERC20 token with minting capabilities.
+ * You'll be "selling" the tokens this contract generates to your classmates using your crowdsale contract.
+ * Make sure to inherit functionality from the MintableToken contract.
+ * Rename the contract file to the name of your custom token (i.e. EricToken.sol or BuidlDApps.sol or RocketCoin.sol)
+ */ 
+ 
+// Set up your contract to inherit functions from your MintableToken contract. 
+contract {
+    // Define 3 public state variables: name, symbol, number of decimals.
+    string public constant name = ;
+    string public constant symbol = ;
+    uint8 public constant decimals = 18; //
+}
